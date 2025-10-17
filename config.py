46df3db7eeafadccb5d19fcec577f974efe8bc46
Xyrenzo <166607781+Xyrenzo@users.noreply.github.com>
2025-10-17 23:20:21 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent
 DB_PATH = BASE_DIR / "users.db"
 TEMPLATES_DIR = BASE_DIR / "templates"
+GEMINI_API_KEY = os.getenv("API_KEY")
